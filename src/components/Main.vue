d05af28c9adc6e0e5d9d4dd29c13f0f5ac518e08
Rafhael <rafhaelalvesprado@gmail.com>
2025-01-03 14:17:33 -0300

    adicionado components

---
+++ src/components/Main.vue
@@ -0,0 +1,51 @@
+<script setup>
+const props = defineProps(['NumerosObj', 'CalcularFunc'])
+</script>
+
+<template>
+    <div class="Main">
+        <div class="Numbers">
+            <input :value="NumerosObj.numeroA" @keyup="evento => CalcularFunc(evento.target.value, 'A')" type="number"
+                placeholder="0">
+            <input :value="NumerosObj.numeroB" @keyup="evento => CalcularFunc(evento.target.value, 'B')" type="number"
+                placeholder="0">
+        </div>
+        <div class="Reslt">
+            <h2>{{ NumerosObj.NumeroDisplay }}</h2>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+.Main {
+    padding-top: 35px;
+}
+
+input {
+    margin: 0 25px;
+}
+
+.Simbulos {
+    display: flex;
+    justify-content: center;
+}
+
+h1 {
+    text-align: center;
+}
+
+.Reslt {
+    display: flex;
+    justify-content: center;
+}
+
+h2 {
+    margin-top: 20px;
+    text-align: right;
+    background-color: gray;
+    border-radius: 10px;
+    padding: 20px 0;
+    width: 200px;
+}
+</style>
---
+++ src/components/Simbulos.vue
@@ -0,0 +1,27 @@
+<script setup>
+const props = defineProps(['NumerosObj'])
+</script>
+
+<template>
+    <div class="Simbulos">
+        <select
+            @change="evento => { NumerosObj.Simbulo = evento.target.value, NumerosObj.NumeroDisplay = '0', NumerosObj.numeroA = '', NumerosObj.numeroB = '' }">
+            <option value="+">+</option>
+            <option value="-">-</option>
+            <option value="*">x</option>
+            <option value="/">/</option>
+        </select>
+    </div>
+</template>
+
+<style scoped>
+.Simbulos {
+    display: flex;
+    justify-content: center;
+    padding: 50px 0;
+}
+
+select {
+    text-align: center;
+}
+</style>
